646f3ed32826d7e3f76d1a03567f5ada15d2cafa
dannnyliang <youchenliang@gmail.com>
2020-07-20 00:38:01 +0800

    feat: fetch more when scroll to list page bottom

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -4,19 +4,32 @@ import {
   Grid,
   Typography,
 } from "@material-ui/core";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import CollectionCard from "../components/CollectionCard";
 import { fetchCollections } from "../store/actions";
 import { FETCHS_TATUS } from "../store/constant";
 
+const isReachBottom = (element) =>
+  element.getBoundingClientRect().bottom <= window.innerHeight;
+
 const List = () => {
   const dispatch = useDispatch();
   const collections = useSelector((state) => state.collections);
   const fetchStatus = useSelector((state) => state.fetchStatus);
+  const hasMoreCollections = useSelector((state) => state.hasMoreCollections);
+  const listRef = useRef(null);
 
   const isFetching = fetchStatus === FETCHS_TATUS.PENDING;
+
+  const trackScrolling = useCallback(() => {
+    if (listRef.current) {
+      if (isReachBottom(listRef.current) && !isFetching && hasMoreCollections) {
+        dispatch(fetchCollections({ offset: collections.length }));
+      }
+    }
+  }, [collections.length, dispatch, hasMoreCollections, isFetching]);
 
   useEffect(() => {
     if (collections.length === 0) {
@@ -24,12 +37,19 @@ const List = () => {
     }
   }, [collections.length, dispatch]);
 
+  useEffect(() => {
+    document.addEventListener("scroll", trackScrolling);
+    return () => {
+      document.removeEventListener("scroll", trackScrolling);
+    };
+  }, [trackScrolling]);
+
   return (
     <Container>
       <Typography align="center" variant="h3">
         收藏品列表
       </Typography>
-      <Grid container spacing={3}>
+      <Grid innerRef={listRef} container spacing={3}>
         {collections.map((collection) => (
           <Grid item xs={6} sm={4} md={3}>
             <CollectionCard
@@ -43,6 +63,7 @@ const List = () => {
         ))}
       </Grid>
       {isFetching && <CircularProgress />}
+      {!hasMoreCollections && <div>no more collections</div>}
     </Container>
   );
 };
